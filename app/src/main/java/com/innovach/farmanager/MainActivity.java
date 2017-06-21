69ae9f79162ed6844c413dcaa94d6952816548a8
viv-iam <vivian.opondoh@gmail.com>
2017-06-21 06:22:55 +0300

    Add activities *Create FrenchBeans, Maize, Onions, Potatoes and Tomatoes activities *Add code to onItemClick on MainActitvity that takes one to the different activities upon click of the different items

--- app/src/main/java/com/innovach/farmanager/MainActivity.java
+++ app/src/main/java/com/innovach/farmanager/MainActivity.java
@@ -1,6 +1,7 @@
 package com.innovach.farmanager;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.res.TypedArray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +9,12 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import com.innovach.farmanager.produce.details.FrenchBeansActivity;
+import com.innovach.farmanager.produce.details.MaizeActivity;
+import com.innovach.farmanager.produce.details.OnionsActivity;
+import com.innovach.farmanager.produce.details.PotatoesActivity;
+import com.innovach.farmanager.produce.details.TomatoesActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -48,11 +55,30 @@ public class MainActivity extends Activity implements AdapterView.OnItemClickLis
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position,
                             long id) {
+//
+//        String member_name = rowItems.get(position).getProduce_name();
+//        Toast.makeText(getApplicationContext(), "" + member_name,
+//                Toast.LENGTH_SHORT).show();
+        if (position == 0) {
+            Intent myIntent = new Intent(view.getContext(), TomatoesActivity.class);
+            startActivityForResult(myIntent, 0);
+        }
+        if (position == 1) {
+            Intent myIntent = new Intent(view.getContext(), OnionsActivity.class);
+            startActivityForResult(myIntent, 1);
+        }
+        if (position == 2) {
+            Intent myIntent = new Intent(view.getContext(), MaizeActivity.class);
+            startActivityForResult(myIntent, 2);
+        }
+        if (position == 3) {
+            Intent myIntent = new Intent(view.getContext(), PotatoesActivity.class);
+            startActivityForResult(myIntent, 3);
+        }
+        if (position == 4) {
+            Intent myIntent = new Intent(view.getContext(), FrenchBeansActivity.class);
+            startActivityForResult(myIntent, 4);
+        }
 
-        String member_name = rowItems.get(position).getProduce_name();
-        Toast.makeText(getApplicationContext(), "" + member_name,
-                Toast.LENGTH_SHORT).show();
     }
-
 }
-//    Contact GitHub API Training Shop Blog About
---
+++ app/src/main/java/com/innovach/farmanager/produce/details/OnionsActivity.java
@@ -0,0 +1,15 @@
+package com.innovach.farmanager.produce.details;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.innovach.farmanager.R;
+
+public class OnionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onions);
+    }
+}
