44b9d190185e0f2442f2fab83a0521854297105e
viv-iam <vivian.opondoh@gmail.com>
2017-06-21 08:04:09 +0300

    Add DiseaseActivity

--- app/src/main/java/com/innovach/farmanager/DiseaseActivity.java
+++ app/src/main/java/com/innovach/farmanager/DiseaseActivity.java
@@ -8,6 +8,6 @@ public class DiseaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_disease);
+        setContentView(R.layout.activity_diseas);
     }
 }
